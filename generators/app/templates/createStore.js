97b9e79a03e316a728226a1d14b0b883495bec1b
Julia Ricketti <julsricketti@gmail.com>
2018-03-17 09:12:11 -0400

    Update createStore to be more complete

--- generators/app/templates/createStore.js
+++ generators/app/templates/createStore.js
@@ -7,10 +7,21 @@ import {
 import { routerReducer } from 'react-router-redux'
 import * as reducers from './ducks'
 
-export default function createStore (...middlewares) {
+const allReducers = Object.assign({}, reducers, {
+  routing: routerReducer
+})
+
+const composeEnhancers = typeof __REDUX_DEVTOOLS_EXTENSION_COMPOSE__ === 'function'
+  ? __REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  : compose
+
+  const initialState = typeof __INITIAL_STATE__ === 'object'
+  ? __INITIAL_STATE__
+  : {}
+
+export default function createStore () {
   return doCreateStore(
     combineReducers(allReducers),
-    initialState,
-    composeEnhancers(applyMiddleware(...middlewares))
+    initialState
   )
 }
